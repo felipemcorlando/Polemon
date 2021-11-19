e268ad2b9e5bc61423fd255838170f076f8ff28a
f170926 <f170926@dac.unicamp.br>
2021-11-19 16:20:20 -0300

    :tada: Adds initial package and classes

---
+++ Polemon/src/com/unicamp/br/mc322/polemon/Game.java
@@ -0,0 +1,17 @@
+package com.unicamp.br.mc322.polemon;
+
+public class Game {
+
+	private boolean exitSelected;
+	
+	public void start() {
+		exitSelected = false;
+		System.out.println("Game started!");
+		while(!exitSelected) {
+			//drawBoard();
+			//readInput();
+			//updateBoard();
+		}
+		System.out.println("Game terminated. Bye!");
+	}
+}
---
+++ Polemon/src/com/unicamp/br/mc322/polemon/Runner.java
@@ -0,0 +1,8 @@
+package com.unicamp.br.mc322.polemon;
+
+public class Runner {
+	public static void main(String[] args) {
+		Game g = new Game();
+		g.start();
+	}
+}
