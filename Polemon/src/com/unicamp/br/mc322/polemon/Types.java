15f7f8ebdcaf5d158abd0a04e6654421b862478f
f170926 <f170926@dac.unicamp.br>
2021-11-22 15:05:23 -0300

    :sparkles: Adds Types Enum

---
+++ Polemon/src/com/unicamp/br/mc322/polemon/Types.java
@@ -0,0 +1,5 @@
+package com.unicamp.br.mc322.polemon;
+
+public enum Types {
+	Fire, Grass, Water, Eletric, Dragon, Psychic, Poison, Ice, Rock, Ground, Dark, Flying, Bug, Normal, Fight, Steel, Ghost;
+}
