91f42a611408fb70d412180feaf852613417c2bd
f170926 <f170926@dac.unicamp.br>
2021-11-23 12:54:21 -0300

    :construction: Adds typesRelation method to Types Enum

--- Polemon/src/com/unicamp/br/mc322/polemon/Types.java
+++ Polemon/src/com/unicamp/br/mc322/polemon/Types.java
@@ -2,4 +2,47 @@ package com.unicamp.br.mc322.polemon;
 
 public enum Types {
 	Fire, Grass, Water, Eletric, Normal;
+
+	public double typesRelations(Types attackingType, Types defendingType){
+		/*This method returns a multiplier to be applied in the damage given to the defending
+		 * pokemon*/
+		if (defendingType == Types.Fire) {
+			if(attackingType == Types.Water)
+				return 2;
+			else if(attackingType == Types.Fire)
+				return 0.5;
+			else if(attackingType == Types.Grass)
+				return 0.5;
+			else
+				return 1;
+		}else if(defendingType == Types.Water) {
+			if(attackingType == Types.Fire)
+				return 0.5;
+			else if(attackingType == Types.Water)
+				return 0.5;
+			else if(attackingType == Types.Grass)
+				return 2;
+			else if(attackingType == Types.Eletric)
+				return 2;
+			else
+				return 1;
+		}else if(defendingType == Types.Grass) {
+			if(attackingType == Types.Fire)
+				return 2;
+			else if(attackingType == Types.Water)
+				return 0.5;
+			else if(attackingType == Types.Grass)
+				return 0.5;
+			else if(attackingType == Types.Eletric)
+				return 0.5;
+			else
+				return 1;
+		}else if(defendingType == Types.Eletric) {
+			if(attackingType == Types.Eletric)
+				return 0.5;
+			else
+				return 1;
+		}else
+			return 1;
+	}
 }
