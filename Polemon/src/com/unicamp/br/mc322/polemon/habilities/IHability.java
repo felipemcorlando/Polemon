16b50397291843f5700c2d56ada664aee436b2f8
f170926 <f170926@dac.unicamp.br>
2021-11-23 19:11:31 -0300

    :construction: Adds useHability method to Ihability interface

--- Polemon/src/com/unicamp/br/mc322/polemon/habilities/IHability.java
+++ Polemon/src/com/unicamp/br/mc322/polemon/habilities/IHability.java
@@ -1,5 +1,9 @@
 package com.unicamp.br.mc322.polemon.habilities;
 
+import com.unicamp.br.mc322.polemon.Pokemon;
+
 public interface IHability {
 
+	public void useHability(Pokemon attacker, Pokemon defender);
+	
 }
