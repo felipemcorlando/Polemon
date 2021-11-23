7fc0ba294a99af2a7240104d9f4f839385c5aec1
wyattgustavo <wyattgustavo@gmail.com>
2021-11-22 22:52:42 -0300

    :sparkles:Implements Dice

--- Polemon/src/com/unicamp/br/mc322/polemon/Dice.java
+++ Polemon/src/com/unicamp/br/mc322/polemon/Dice.java
@@ -1,5 +1,12 @@
 package com.unicamp.br.mc322.polemon;
 
-public class Dice {
-//essa é uma classe static
+public final class Dice {
+
+	public static int roll(int faces, int nDices) {
+		int total = 0;
+		for (int i = 0; i < nDices; i++)
+			total += 1 + (int)(Math.random() * faces);
+		
+		return total;
+	}
 }
