f2128dfd19b87789dcab2c982681c9c8b8d5484e
f170926 <f170926@dac.unicamp.br>
2021-11-23 20:27:25 -0300

    :adhesive_bandage: Fix a possible bug

--- Polemon/src/com/unicamp/br/mc322/polemon/habilities/ActiveHability.java
+++ Polemon/src/com/unicamp/br/mc322/polemon/habilities/ActiveHability.java
@@ -21,6 +21,9 @@ public class ActiveHability implements IHability{
 		double multiplier = type.typesRelations(type, defender.getType());
 		double damageDealt = this.damage*multiplier + attacker.getAttackPoints() - defender.getDefensePoints();
 		//O multiplicador referente a relação de tipos apenas multiplica o dano da habilidade (atributo damage);
+		if (damageDealt < 0)
+			damageDealt = 0;
+		//Isso é apenas para evitar valores negativos que causariam problemas.
 		defender.setHp(defender.getHp() - damageDealt);
 	}
 
