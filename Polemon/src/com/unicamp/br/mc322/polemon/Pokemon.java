44e142445e0fa98404cc247629a7c62bfd1bcd88
f170926 <f170926@dac.unicamp.br>
2021-11-23 20:13:38 -0300

    :construction: Introduces and fixes features in Pokemon class

--- Polemon/src/com/unicamp/br/mc322/polemon/Pokemon.java
+++ Polemon/src/com/unicamp/br/mc322/polemon/Pokemon.java
@@ -1,9 +1,44 @@
 package com.unicamp.br.mc322.polemon;
 
 public class Pokemon {
-	Types type;
+	
+	private Types type;
+	private int hp; //Must be a positive value;
+	private int attackPoints; //Must be a value between 0 and 15;
+	private int defensePoints; //Must be a value between 0 and 15;
+	
+	public Pokemon(Types type, int newHP, int newAttackPoints, int newDefensePoints) throws IllegalValueException{
+		this.type = type;
+		if(newHP < 0) {
+			this.hp = 0;
+			throw new IllegalValueException("Hit Points must be a positive value");
+		}
+		if(newAttackPoints > 15 || newAttackPoints < 0) {
+			this.attackPoints = 0;
+			throw new IllegalValueException("Attack Points must be between 0 and 15");
+		}
+		if(newDefensePoints > 15 || newDefensePoints < 0) {
+			this.defensePoints = 0;
+			throw new IllegalValueException("Defense Points must be between 0 and 15");
+		}	
+	}
 	
 	public Types getType() {
 		return type;
+	}
+
+
+	public int getHp() {
+		return hp;
+	}
+
+
+	public int getAttackPoints() {
+		return attackPoints;
+	}
+
+
+	public int getDefensePoints() {
+		return defensePoints;
 	}
 }
