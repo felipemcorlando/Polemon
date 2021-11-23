4fbe6fcb17346bab800e2627e6a3d2d520ea1ec0
f170926 <f170926@dac.unicamp.br>
2021-11-23 19:34:01 -0300

    :construction: Adds constructor and attributes to ActiveHability

--- Polemon/src/com/unicamp/br/mc322/polemon/habilities/ActiveHability.java
+++ Polemon/src/com/unicamp/br/mc322/polemon/habilities/ActiveHability.java
@@ -1,9 +1,20 @@
 package com.unicamp.br.mc322.polemon.habilities;
 
 import com.unicamp.br.mc322.polemon.Pokemon;
+import com.unicamp.br.mc322.polemon.Types;
 
 public class ActiveHability implements IHability{
 
+	private String name;
+	private float damage;
+	private Types type;
+	
+	public ActiveHability(String name, float damage, Types type) {
+		this.name = name;
+		this.damage = damage;
+		this.type = type;
+	}
+	
 	@Override
 	public void useHability(Pokemon attacker, Pokemon defender) {
 		// TODO Auto-generated method stub
