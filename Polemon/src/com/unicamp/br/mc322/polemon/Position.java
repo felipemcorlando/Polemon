0bc81a3b1fa89fcfc2d66df8921d18ad20dee33e
wyattgustavo <wyattgustavo@gmail.com>
2021-11-24 12:13:58 -0300

    :sparkles: Implements Position

--- Polemon/src/com/unicamp/br/mc322/polemon/Position.java
+++ Polemon/src/com/unicamp/br/mc322/polemon/Position.java
@@ -1,5 +1,43 @@
 package com.unicamp.br.mc322.polemon;
 
 public class Position {
+	private int x, y, z;
+	
+	public Position(int x, int y, int z) {
+		this.setX(x);
+		this.setY(y);
+		this.setZ(z);
+	}
 
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getZ() {
+		return z;
+	}
+
+	public void setZ(int z) {
+		this.z = z;
+	}
+	
+	public Position travel(Position distance) {
+		this.x += distance.getX();
+		this.y += distance.getY();
+		this.z += distance.getZ();
+		
+		return this;
+	}
 }
