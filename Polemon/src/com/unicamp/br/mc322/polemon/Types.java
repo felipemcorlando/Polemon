821731fd41b8966a3dfba11a9da1db2d7a83b9c9
f170926 <f170926@dac.unicamp.br>
2021-11-25 14:24:00 -0300

    :fire: Delete typesRelations method that should not exist.

--- Polemon/src/com/unicamp/br/mc322/polemon/Types.java
+++ Polemon/src/com/unicamp/br/mc322/polemon/Types.java
@@ -1,48 +1,5 @@
 package com.unicamp.br.mc322.polemon;
 
 public enum Types {
-	Fire, Grass, Water, Electric, Normal;
-
-	public double typesRelations(Types attackingType, Types defendingType){
-		/*This method returns a multiplier to be applied in the damage given to the defending
-		 * pokemon*/
-		if (defendingType == Types.Fire) {
-			if(attackingType == Types.Water)
-				return 2;
-			else if(attackingType == Types.Fire)
-				return (float) 0.5;
-			else if(attackingType == Types.Grass)
-				return 0.5;
-			else
-				return 1;
-		}else if(defendingType == Types.Water) {
-			if(attackingType == Types.Fire)
-				return 0.5;
-			else if(attackingType == Types.Water)
-				return 0.5;
-			else if(attackingType == Types.Grass)
-				return 2;
-			else if(attackingType == Types.Electric)
-				return 2;
-			else
-				return 1;
-		}else if(defendingType == Types.Grass) {
-			if(attackingType == Types.Fire)
-				return 2;
-			else if(attackingType == Types.Water)
-				return 0.5;
-			else if(attackingType == Types.Grass)
-				return 0.5;
-			else if(attackingType == Types.Electric)
-				return 0.5;
-			else
-				return 1;
-		}else if(defendingType == Types.Electric) {
-			if(attackingType == Types.Electric)
-				return 0.5;
-			else
-				return 1;
-		}else
-			return 1;
-	}
+	Fire, Grass, Water, Electric, Psychic, Dragon;
 }
