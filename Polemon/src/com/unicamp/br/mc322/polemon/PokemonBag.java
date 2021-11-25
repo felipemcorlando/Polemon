56f573375e7f07ed5000ae9f31c78d545d8ff791
f170926 <f170926@dac.unicamp.br>
2021-11-25 17:48:42 -0300

    :sparkles: Adds PokemonBag and implements it

---
+++ Polemon/src/com/unicamp/br/mc322/polemon/PokemonBag.java
@@ -0,0 +1,34 @@
+package com.unicamp.br.mc322.polemon;
+
+import java.util.ArrayList;
+
+public class PokemonBag {
+
+	private ArrayList<Pokemon> pokemonList = new ArrayList<Pokemon>();
+	
+	public void addItem(Pokemon pokemon) {
+		this.pokemonList.add(pokemon);
+	}
+	
+	public void removeItem(Pokemon pokemon) {
+		this.pokemonList.remove(pokemon);
+	}
+	
+	public Pokemon getItem(String name) {
+		Pokemon pokemon = null;
+		for(Pokemon p : this.pokemonList) {
+			if(p.getName() == name) {
+				pokemon = p;
+			}
+		}
+		return pokemon;
+	}
+	
+	public String toString() {
+		String s = "";
+		for(Pokemon p : this.pokemonList) {
+			s = s + p.getName()+ " ";
+		}
+		return s;
+	}
+}
