79d02fc1405b05743f9a29a40094efc1c12e64d9
f170926 <f170926@dac.unicamp.br>
2021-11-25 14:58:28 -0300

    :sparkles: Adds Collectable and initial methods

---
+++ Polemon/src/com/unicamp/br/mc322/polemon/items/Collectable.java
@@ -0,0 +1,12 @@
+package com.unicamp.br.mc322.polemon.items;
+
+import com.unicamp.br.mc322.polemon.Pokemon;
+import com.unicamp.br.mc322.polemon.Position;
+
+public interface Collectable {
+
+	public void useItem(Pokemon target);
+	
+	public Position getPosition();
+	
+}
