d8be9ea80a6afd2f277987bc17498628bc04e393
f170926 <f170926@dac.unicamp.br>
2021-11-26 18:37:35 -0300

    :construction: Implements player

--- Polemon/src/com/unicamp/br/mc322/polemon/Player.java
+++ Polemon/src/com/unicamp/br/mc322/polemon/Player.java
@@ -1,16 +1,70 @@
 package com.unicamp.br.mc322.polemon;
 
-import com.unicamp.br.mc322.polemon.map.Island;
+import com.unicamp.br.mc322.polemon.items.*;
+import com.unicamp.br.mc322.polemon.map.*;
+
+import java.util.*;
 
 public class Player {
-	private Island visitedIslands[];
-	private Position position;
+
+	private Position globalPosition;
+	private Pokemon activatedPokemon;
 	
-	public void changePosition(Position newPosition) {
-		position=newPosition;
-		return;
+	private PokemonBag minePokemons;	
+	private Inventory inventory;
+	private ArrayList<Island> visitedIslands;
+	private boolean inCombat;
+	
+	public Player() {
+		this.globalPosition = new Position(1, 1, 0);
 	}
+
+	public boolean isInCombat() {
+		return this.inCombat;
+	}
+	
+	public Collectable collectItem(Plan actualPlan) {
+		//checks if there is an item in the same position of player, if true collects
+		Island island = actualPlan.findIsland(this.globalPosition);
+		for (int i = 0; i < island.getCollectables().size(); i++) {
+			Collectable item = island.getCollectables().get(i);
+			if (this.globalPosition.equals(item.getPosition())) {
+				inventory.addItem(item);
+				island.getCollectables().remove(i);
+				return item;
+			}
+		}
+		
+		return null;
+	}
+	
+	public boolean choosePokemon(int i) {
+		Pokemon p = this.minePokemons.getPokemonList().get(i);
+		if (p == null)
+			return false;
+		
+		this.activatedPokemon = p;
+		return true;
+	}
+	
 	public Position getGlobalPosition() {
-		return position;
+		return globalPosition;
 	}
+
+	public void setGlobalPosition(Position globalPosition) {
+		this.globalPosition = globalPosition;
+	}
+
+	public PokemonBag getMinePokemons() {
+		return minePokemons;
+	}
+
+	public Inventory getInventory() {
+		return inventory;
+	}
+
+	public void setInventory(Inventory inventory) {
+		this.inventory = inventory;
+	}
+	
 }
