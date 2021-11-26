5c5b3ce83800bd3f5b7ea5e609f3922289d2c94f
f170926 <f170926@dac.unicamp.br>
2021-11-26 14:05:51 -0300

    :sparkles: Adds getType in Types enum

--- Polemon/src/com/unicamp/br/mc322/polemon/Types.java
+++ Polemon/src/com/unicamp/br/mc322/polemon/Types.java
@@ -1,5 +1,24 @@
 package com.unicamp.br.mc322.polemon;
 
 public enum Types {
-	Fire, Grass, Water, Electric, Psychic, Dragon;
+	FIRE, GRASS, WATER, ELECTRIC, PSYCHIC, DRAGON;
+	
+	public Types getType(int index) { //Método para usado para pegar um método aleatório para a ilha.
+		switch(index) {
+		case 0:
+			return Types.FIRE;
+		case 1:
+			return Types.GRASS;
+		case 2:
+			return Types.WATER;
+		case 3:
+			return Types.ELECTRIC;
+		case 4:
+			return Types.PSYCHIC;
+		case 5:
+			return Types.DRAGON;
+		default:
+			return null;
+		}
+	}
 }
