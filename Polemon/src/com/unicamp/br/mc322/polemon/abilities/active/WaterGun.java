15b61928987c4cc5caa3afb26d9c5f135592bfbf
f170926 <f170926@dac.unicamp.br>
2021-11-29 17:15:46 -0300

    :sparkles: Adds all new active abilities

---
+++ Polemon/src/com/unicamp/br/mc322/polemon/abilities/active/WaterGun.java
@@ -0,0 +1,57 @@
+package com.unicamp.br.mc322.polemon.abilities.active;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import com.unicamp.br.mc322.polemon.Pokemon;
+import com.unicamp.br.mc322.polemon.Types;
+
+public class WaterGun implements IActiveAbility {
+
+	private String name = "WaterGun"; //Habilidade comum
+	private int damage = 40;
+	private int accuracy = 100;
+	private int criticalHitAcc = 5;
+	private ArrayList<Types> typesAllowed = new ArrayList<Types>();
+
+	public WaterGun() {
+		this.typesAllowed.add(Types.WATER);
+		this.typesAllowed.add(Types.DRAGON);
+	}
+	
+	@Override
+	public void useHability(Pokemon attacker, Pokemon defender) {
+		// TODO Auto-generated method stub
+		double damageDealt = 0;
+		Random rand = new Random();
+		if(rand.nextInt(100) < this.accuracy) { //Ataque acertou
+			//Algum feedback caso acertou?
+			double extraDamage = 1;
+			if(rand.nextInt(100) < this.criticalHitAcc) //Teve critical Hit
+				//Algum feedback caso acertou critcial hit?
+				extraDamage = 1.5;
+			damageDealt = extraDamage*this.damage;
+			defender.setHp(defender.getHp()-damageDealt);
+		}
+	}
+
+	@Override
+	public boolean canLearn(Pokemon learner) {
+		// TODO Auto-generated method stub
+		for(Types pokemon : learner.getPokemonType()) {
+			for(Types allowed : this.typesAllowed) {
+				if(pokemon == allowed) {
+					return true; //O Pokemon consegue aprender a habilidade
+				}
+			}
+		}
+		return false; //O Pokemon não consegue aprender a habilidade
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return this.name;
+	}
+
+}
