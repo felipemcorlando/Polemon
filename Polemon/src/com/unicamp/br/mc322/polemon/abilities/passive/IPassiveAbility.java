bc5edb023cdc222ef543b99a1ec91125f21e3ec2
f170926 <f170926@dac.unicamp.br>
2021-11-30 12:59:40 -0300

    :construction: Refactor file name

--- Polemon/src/com/unicamp/br/mc322/polemon/abilities/passive/IPassiveAbility.java
+++ Polemon/src/com/unicamp/br/mc322/polemon/abilities/passive/IPassiveAbility.java
@@ -2,6 +2,6 @@ package com.unicamp.br.mc322.polemon.abilities.passive;
 
 import com.unicamp.br.mc322.polemon.abilities.IAbility;
 
-public interface IPassiveHability extends IAbility {
+public interface IPassiveAbility extends IAbility {
 
 }
