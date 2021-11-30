07481dda44e90ecb3100077a06776a4656c8c332
f170926 <f170926@dac.unicamp.br>
2021-11-30 12:58:36 -0300

    :construction: changes to abstract class

--- Polemon/src/com/unicamp/br/mc322/polemon/items/TechnicalRecord.java
+++ Polemon/src/com/unicamp/br/mc322/polemon/items/TechnicalRecord.java
@@ -1,23 +1,12 @@
 package com.unicamp.br.mc322.polemon.items;
 
-import com.unicamp.br.mc322.polemon.Pokemon;
 import com.unicamp.br.mc322.polemon.Position;
-import com.unicamp.br.mc322.polemon.abilities.IAbility;
 
-public class TechnicalRecord implements Collectable {
+public abstract class TechnicalRecord implements Collectable {
 
 	private String name;
 	private Position position;
-	private IAbility ability;
 	
-	@Override
-	public void useItem(Pokemon target) {
-		// TODO Auto-generated method stub
-		if(ability.canLearn(target)) {
-			target.teachAbility(ability);
-		}
-		//Usar método do gustavo para printar erro.
-	}
 
 	@Override
 	public Position getPosition() {
