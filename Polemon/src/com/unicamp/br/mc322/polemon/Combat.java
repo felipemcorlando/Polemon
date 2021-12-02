19267a0d0195e4a16fd863cb2439c4031ce3e9f5
Polezim <g171083@dac.unicamp.br>
2021-12-02 16:22:35 -0300

    Starting Combat

--- Polemon/src/com/unicamp/br/mc322/polemon/Combat.java
+++ Polemon/src/com/unicamp/br/mc322/polemon/Combat.java
@@ -10,10 +10,45 @@ public class Combat {
 		this.player = pl;
 		this.targetPokemon = po;
 		this.firstPlayer = isPlayerFirst;
+
 	}
 	
 	public void drawCombat () {
-		
+		while (player.getActivatedPokemon().getHp()>0 && targetPokemon.getHp()>0) {
+			shift(player,targetPokemon,firstPlayer) ;
+		}
 	}
 	
+	
+	
+	
+	public void shift(Player pl, Pokemon po, boolean isPlayerFirst) {//turno de ataque
+		if (isPlayerFirst) {
+			//player ataca primeiro 
+			playerAttack(pl,po);
+			if(po.getHp()<1)
+				return;
+			else
+				pokemonAttack(pl,po);
+		}
+		else {
+			//pokemon ataca primeiro
+			pokemonAttack(pl,po);
+			//adicionar um check player.getActivatedPokemon().getHp()>0
+			playerAttack(pl,po);
+		}
+	}
+	
+	public void playerAttack(Player pl, Pokemon po) {
+		System.out.println("\nBase attack , Press 0");
+		System.out.println("\nChoose one ability , Press 1");
+		try {
+			int command = Integer.parseInt(Input.readKeyboard());
+			;
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}		
+		
+	}
 }
