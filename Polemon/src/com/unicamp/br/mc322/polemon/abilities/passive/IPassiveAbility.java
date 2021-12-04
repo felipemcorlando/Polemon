20ad9304e9e4aeeff6714f672883d648e79be478
f170926 <f170926@dac.unicamp.br>
2021-12-04 17:53:44 -0300

    :bulb: Add comment in IPassiveAbility

--- Polemon/src/com/unicamp/br/mc322/polemon/abilities/passive/IPassiveAbility.java
+++ Polemon/src/com/unicamp/br/mc322/polemon/abilities/passive/IPassiveAbility.java
@@ -4,7 +4,7 @@ import com.unicamp.br.mc322.polemon.Pokemon;
 
 public interface IPassiveAbility {
 
-	public double useHability(double d,Pokemon self, Pokemon opponent);
+	public double useHability(double d,Pokemon self, Pokemon opponent); //Esse double d é passado pois algumas passivas modificam o dano.
 	
 	public boolean canLearn(Pokemon learner);
 	
